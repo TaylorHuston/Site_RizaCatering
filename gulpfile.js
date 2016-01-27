1262b762062f8ef5fd79edb1df8d28832ff0ce53
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-27 04:40:11 -0700

    Add basic demo Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
 
 gulp.task('default', function() {
   // place code for your default task here
+  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+  return gutil.log('Gulp is running!');
 });
