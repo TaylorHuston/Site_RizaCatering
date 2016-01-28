909d00f3aa62a98bd2c306f0602cb999e7d2f747
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-28 00:50:07 -0700

    commit to change machines

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint');
 
 
-gulp.task('default', function() {
+gulp.task('default', ['copy'], function() {
   // place code for your default task here
-  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+  
   return gutil.log('Gulp is running!');
+});
+
+gulp.task('copy', function() {
+  gulp.src('src/*.html').pipe(gulp.dest('dist'));
 });
