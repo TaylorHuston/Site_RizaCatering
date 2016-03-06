25288d07cf16eb50c391870b15372223f8d27f10
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-05 21:06:02 -0700

    Test using gulp-file-include on home page

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    fileinclude = require('gulp-file-include');
 
-
-gulp.task('default', ['copy'], function() {
+gulp.task('default', ['fileinclude'], function() {
   // place code for your default task here
-  
+
   return gutil.log('Gulp is running!');
 });
 
 gulp.task('copy', function() {
-  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+//  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('fileinclude', function() {
+  gulp.src('src/index.html')
+  .pipe(fileinclude({
+    prefix: '@@',
+    basepath: '@file'
+  }))
+  .pipe(gulp.dest('dist'));
 });
